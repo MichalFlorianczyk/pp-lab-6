b79da34daa38ba52a1dd548da9926fcd7e2339b0
MichalFlorianczyk <166236193+MichalFlorianczyk@users.noreply.github.com>
2024-05-05 19:37:19 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 
 import company.abstracts.Employee;
@@ -6,26 +7,27 @@ import company.models.Worker;
 
 public class Main {
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Maciej", 6000, 1, "2022-01-01", "Junior Developer");
-        Worker worker2 = new Worker("Robert", 4800, 2, "2023-03-15", "Senior Developer");
-        Worker worker3 = new Worker("Kamil", 9400, 3, "2020-12-10", "Tester");
-        Worker worker4 = new Worker("Andrzej", 8400, 4, "2021-06-20", "Project Manager");
 
-        Manager manager = new Manager("Michael", 5000, 5, "2019-05-10", "Team Head");
+        Worker worker1 = new Worker("Maciej", 6000.0, 1, "2022-01-01", "Junior Developer");
+        Worker worker2 = new Worker("Robert", 4800.0, 2, "2023-03-15", "Senior Developer");
+        Worker worker3 = new Worker("Kamil", 9400.0, 3, "2020-12-10", "Tester");
+        Manager manager = new Manager("Michael", 5000.0, 5, "2019-05-10", "Team Head");
 
         ArrayList<Employee> employees = new ArrayList<>();
         employees.add(worker1);
         employees.add(worker2);
         employees.add(worker3);
-
         employees.add(manager);
 
-        for (Employee employee : employees) {
-            System.out.println("- " + employee.getName() + " (ID: " + employee.hashCode() +
-                    ", Position: " + employee.getPosition() +
-                    ", Hire date: " + employee.getHireDate() +
-                    ", Salary: " + employee.getSalary() + ")");
-            employee.work();
+        for (Employee emp : employees) {
+            System.out.println(emp.getName() + " (ID: " + emp.hashCode() + ") has code: " + emp.hashCode());
+        }
+
+        System.out.println("Comparing workers with the same ID:");
+        for (Employee emp : employees) {
+            if (worker1.equals(emp)) {
+                System.out.println(worker1.getName() + " and " + emp.getName() + " are equal.");
+            }
         }
     }
 }
--- company/abstracts/Employee.java
+++ company/abstracts/Employee.java
@@ -6,8 +6,8 @@ public abstract class Employee implements Employable {
     private String name;
     private double salary;
     private int id;
-    private String hireDate; // Nowe pole
-    private String position; // Nowe pole
+    private String hireDate;
+    private String position;
 
     public Employee(String name, double salary, int id, String hireDate, String position) {
         this.name = name;
@@ -15,14 +15,6 @@ public abstract class Employee implements Employable {
         this.id = id;
         this.hireDate = hireDate;
         this.position = position;
-    }
-
-    public String getHireDate() {
-        return hireDate;
-    }
-
-    public String getPosition() {
-        return position;
     }
 
     public String getName() {
@@ -33,12 +25,29 @@ public abstract class Employee implements Employable {
         return salary;
     }
 
-    public int getId() {
-        return id;
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public String getPosition() {
+        return position;
     }
 
     @Override
     public int hashCode() {
         return id;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Employee employee = (Employee) obj;
+        return id == employee.id;
+    }
+
 }
